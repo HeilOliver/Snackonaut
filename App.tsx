2eebd9f6eccb0243c4773b4fc6874216be1423f3
whiteclouds7 <65771894+whiteclouds7@users.noreply.github.com>
2022-09-09 16:51:38 +0200

    rebase pull request

--- App.tsx
+++ App.tsx
@@ -31,46 +31,6 @@ declare module "native-base" {
 
 export default function App() {
   const {t} = useTranslation();
-  return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>{t("test")}</Text>
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
-  );
     return (
         <NativeBaseProvider>
             <Center
@@ -83,6 +43,7 @@ export default function App() {
                     <NativeBaseIcon />
                     <Heading size="lg">Welcome to NativeBase</Heading>
                     <HStack space={2} alignItems="center">
+                        <Text>{t("test")}</Text>
                         <Text>Edit</Text>
                         <Box
                             _web={{
