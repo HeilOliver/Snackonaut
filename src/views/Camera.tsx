9084c32ae65e9ea22469f165d11e3e56af6f7e14
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2022-09-09 18:03:04 +0200

    Add React Navigation to project (#6)

---
+++ src/views/Camera.tsx
@@ -0,0 +1,12 @@
+import { Text } from "native-base";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Camera = () => {
+    return (
+        <SafeAreaView>
+            <Text>Camera</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Camera;
