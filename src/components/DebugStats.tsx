87ea539c41c64dbd54a857de25720d53c9fb718b
Alexander Weber <43758870+alexw-at@users.noreply.github.com>
2022-09-09 19:46:40 +0200

    add styling for stats debug

--- src/components/DebugStats.tsx
+++ src/components/DebugStats.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack } from "native-base";
+import { Button, Center, HStack } from "native-base";
 import React, { useContext } from "react";
 import { StatsContext } from "../providers/StatsProvider";
 
@@ -7,17 +7,19 @@ const DebugStats = () => {
         useContext(StatsContext);
 
     return (
-        <HStack space={1}>
-            <Button onPress={() => setSaturation(stats.saturation + 10)}>
-                +10 Saturation
-            </Button>
-            <Button onPress={() => setHydration(stats.hydration + 10)}>
-                +10 Hydration
-            </Button>
-            <Button onPress={() => setEnergy(stats.energy + 10)}>
-                +10 Energy
-            </Button>
-        </HStack>
+        <Center style={{ marginVertical: 10 }}>
+            <HStack space={1}>
+                <Button onPress={() => setSaturation(stats.saturation + 10)}>
+                    +10 Saturation
+                </Button>
+                <Button onPress={() => setHydration(stats.hydration + 10)}>
+                    +10 Hydration
+                </Button>
+                <Button onPress={() => setEnergy(stats.energy + 10)}>
+                    +10 Energy
+                </Button>
+            </HStack>
+        </Center>
     );
 };
 
